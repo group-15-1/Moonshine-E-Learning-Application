cde7d37fdf44a69ec1b9fc3d2f156983afd3cee3
JanithriEpakanda <116995193+JanithriEpakanda@users.noreply.github.com>
2023-12-04 17:31:42 +0530

    Student login php page updated.

--- Login.php
+++ Login.php
@@ -1,9 +1,9 @@
 
 <?php
 $conn = mysqli_connect('localhost', 'root', '');
-$database = mysqli_select_db($conn, 'moonshin');
+$database = mysqli_select_db($conn, 'moonshine');
 
-$encodedData = file_get_contents('php://input');  
+$encodedData = file_get_contents('php://input');  // take data from react native fetch API
 $decodedData = json_decode($encodedData, true);
 
 $UserEmail = $decodedData['email'];
@@ -14,10 +14,6 @@ $SQL = "SELECT * FROM kids WHERE Email = '$UserEmail' AND password = '$UserPW'";
 $result = mysqli_query($conn, $SQL);
 $checkEmail =  mysqli_num_rows($result);
 
-// Check Teachers Email and Password
-$SQLTeacher = "SELECT * FROM teacher WHERE Email = '$UserEmail' AND password = '$UserPW'";
-$resultTeacher = mysqli_query($conn, $SQLTeacher);
-$checkEmailTeacher =  mysqli_num_rows($resultTeacher);
 
 if ($checkEmail != 0 AND $UserPW !=0) {
        if($result-> num_rows==0){
@@ -28,14 +24,6 @@ if ($checkEmail != 0 AND $UserPW !=0) {
        }
     }
  
-else if ($checkEmailTeacher != 0 AND $UserPW !=0) {
-   if($resultTeacher-> num_rows==0){
-    $Message = "Wrong details";
-   }
-   else{
-    $Message = "ok";
-   }
-}
 else {
 $Message = "No account yet";
 }
